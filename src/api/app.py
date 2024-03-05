cf6b78d6cf16ce1c64e804532efdfe0253bb2e3d
c0mrade <u.keizerov@ogn-itinvest.com>
2024-03-06 01:03:50 +0600

    feat: CORS

--- src/api/app.py
+++ src/api/app.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from src.api.routes import is_admin_router, info_router
 
 app = FastAPI(title='API')
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
 app.include_router(is_admin_router)
 app.include_router(info_router)
